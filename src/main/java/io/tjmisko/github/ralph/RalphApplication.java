5c21e77ba1653886b7c2a2a747aa82ca3e469e04
tjmisko <tjmisko@users.noreply.github.com>
2022-11-24 23:52:40 -0800

    first commit

---
+++ src/main/java/io/tjmisko/github/ralph/RalphApplication.java
@@ -0,0 +1,13 @@
+package io.tjmisko.github.ralph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RalphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RalphApplication.class, args);
+	}
+
+}
